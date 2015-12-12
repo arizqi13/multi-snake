3bcf4bbee3da5ce1e9dd4127978512ad4e331e82
tadrill <tadrip@uw.edu>
2015-12-11 18:04:16 -0800

    added in restrictions for keydown

--- public/game.js
+++ public/game.js
@@ -11,6 +11,7 @@ var context = canvas.getContext('2d');
 
 var nickName;
 var id;
+var dir;
 
 $(document).ready(function(){
 	socket.emit('join');
@@ -21,21 +22,25 @@ $(document).keydown(function(e){
 	if(id != undefined && nickName != undefined){
 		var data = {};
 		data.id = id;
-		if(charCode === 38 || charCode === 87) {
+		if((charCode === 38 || charCode === 87) && dir != 'down') {
 		// Up arrow
 			data.direction = 'up';
+			dir = 'up';
 			socket.emit('key',data);
-		} else if(charCode === 40 || charCode === 83) {
+		} else if((charCode === 40 || charCode === 83) && dir != 'up') {
 		// Down arrow
 			data.direction = 'down';
+			dir = 'down';
 			socket.emit('key',data);
-		} else if(charCode === 39 || charCode === 68) {
+		} else if((charCode === 39 || charCode === 68) && dir != 'left') {
 		// Right arrow
 			data.direction = 'right';
+			dir = 'right';
 			socket.emit('key',data);
-		} else if(charCode === 37 || charCode === 65) {
+		} else if((charCode === 37 || charCode === 65) && dir != 'right') {
 		// Left arrow
 			data.direction = 'left';
+			dir = 'left';
 			socket.emit('key',data);
 		} else {
 		// Not a control key
@@ -45,6 +50,7 @@ $(document).keydown(function(e){
 });
 
 socket.on('incoming data', function(data){
+	// TODO: set initial dir (global var for direction)
 	for(var i=0;i<data.players.length;i++){
 		var player = data.players[i];
 		for(currId in player){
